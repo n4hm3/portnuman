9402ba1ecea069612857bc6ed7ef4b0a27147641
Numan Ahmed <nahmedshaon@gmial.com>
2023-09-06 18:32:08 +0100

    adding the portfolio files

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,116 @@
-import Image from 'next/image'
+import Image from "next/image"
+import pfp from "./pfp.jpg"
+import p1 from "./p1.png"
+import p2 from "./p2.jpg"
+import p3 from "./p3.png"
+import p4 from "./p4.jpg"
+import {AiFillLinkedin, AiFillGithub, AiFillInstagram} from 'react-icons/ai'
+import {FaPython, FaJs, FaReact, FaHtml5, FaGitAlt, FaCss3Alt, FaHackerrank} from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="text-black font-sans">
+        <head>
+          <title>Numan Ahmed</title>
+        </head>
+
+      <main className="bg-white lg:px-32 md:px-3 sm:px-0">
+        <section className=" bg-white min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-bold font-sans ">Numan Ahmed.</h1>
+            <ul className=" flex items-center">
+              <li><a href="mailto:nahmedshaon@gmail.com" target="_blank" className=" font-bold bg-gradient-to-r bg-red-300 px-1 py-1 rounded-md" >Contact me</a></li>
+            </ul>
+          </nav>
+
+        <div className="flex justify-center">
+
+          <div className=" relative rounded-full w-40 h-40 overflow-hidden">
+            <Image src={pfp} layout="fill" objectFit="cover"/>
+        
+          </div>
+          <div className=" font-mono ps-6 pt-8 md:py-9 md:leading-10 lg:py-12">
+            <h2 className="md:text-xl sm:text-sm">Hi, I am a first year <span className=" text-red-300">computer science </span>student.</h2>
+            <h3>Check out some of my work below ↓ </h3>
+
+          </div> 
+          </div>
+
+
+          <h1 className=" font-mono font-bold pt-24 pb-5 text-3xl flex justify-center">COMPENTENCIES</h1>
+
+          <div className="flex text-5xl justify-center">
+            <li className="flex md:gap-10">
+            <FaPython /> <FaHtml5/> <FaCss3Alt/> <FaJs /> <FaReact /> <FaGitAlt/> </li>
+            
+          </div>
+
+          <h1 className=" font-mono font-bold pt-24 pb-5 text-3xl flex justify-center">PROJECTS</h1>
+
+          <div className="md:flex sm:flex-none lg:gap-10 md:gap-5">
+ 
+          <div className=" md:w-1/2 sm:w-full font-mono border-8 p-10 rounded-xl my-5 shadow-2xl ">
+            <h3 className="text-xl pb-4">Photography Portfolio Site</h3>
+            <Image className="w-9/10 border-4" src={p1} />
+            <p className="pt-4 text-gray-500"> Created using vanilla JavaScript, HTML and CSS. Design prototypes made using Figma </p> 
+            <div>
+            <spam className="bg-red-300 p-1"> <a href="https://aristfe.github.io/portfolio/index.html" target="_blank">Go to site </a></spam>
+            </div>
+          </div>
+
+
+          <div className=" md:w-1/2 sm:w-full font-mono border-8 p-10 rounded-xl my-5 shadow-2xl ">
+            <h3 className="text-xl pb-4"> Flower Recognition AI </h3>
+            <Image className=" w-96 border-4" src={p2} />
+              <p className="pt-4 text-gray-500"> Created using Python with TensorFlow, OpenCV, Pandas, Numpy and Matplotlib libraries using a dataset from Kaggle</p> 
+              <spam className="bg-red-300 p-1"> <a href="https://github.com/n4hm3?tab=repositories" target="_blank">GitHub</a></spam>
+          </div>
+                      
+          </div>
+
+
+          <div className=" md:flex justify-center sm:flex-none lg:gap-10 md:gap-5 shadow-2xl ">
+            <div className=" md:w-1/2 sm:w-full font-mono justify-center border-8 p-10 rounded-xl my-5 shadow-2xl">
+              <h3 className="text-xl pb-4">2D Platformer</h3>
+              <Image className="w-9/10 border-4" src={p3} />
+              <p className="pt-4 text-gray-500"> Created using Python with the PyGame Library</p> 
+              <spam className="bg-red-300 p-1"> <a href="https://github.com/n4hm3?tab=repositories" target="_blank">GitHub</a></spam>
+            </div>
+
+            <div className=" md:w-1/2 sm:w-full font-mono justify-center border-8 p-10 rounded-xl my-5 shadow-2xl ">
+              <h3 className="text-xl pb-4">Ecommerce Website</h3>
+              <Image className="w-9/10 border-4 blur-sm" src={p4} />
+              <p className="pt-4 text-gray-500"> Work in progress...</p> 
+            </div>
+
         </div>
-      </div>
+            <div className=" flex justify-between bg-white py-2 ">
+              <p className="text-xs md:pt-10 sm:pt-0">Made by Numan Ahmed using reactJS and tailwindcss</p>
+              <li className=" md:text-5xl text-2xl flex gap-4 ">
+                <a href="https://www.instagram.com/nas4_4/" target="_blank"> <AiFillInstagram /> </a>
+                <a href="https://www.linkedin.com/in/numan-ahmed-711299282/" target="_blank"> <AiFillLinkedin /> </a>
+                <a href="https://github.com/n4hm3" target="_blank"> <AiFillGithub/> </a>
+                <a href="https://www.hackerrank.com/n4hm3" target="_blank"> <FaHackerrank/> </a>
+              </li>
+            </div>
+ 
+          { /* add the names of the competencies
+            <div className="text-xl flex justify-center gap-8 ">
+            <spam className="bg-red-100 ">Python</spam> <spam className="bg-red-100">HTML</spam> <spam className="bg-red-100 ">CSS</spam> <spam className="bg-red-100 ">JavaScript</spam> <spam className="bg-red-100 ">ReactJS</spam> <spam className="bg-red-100 ">Git</spam>
+            </div>
+            <div className=" text-3xl flex justify-center gap-4 ">
+              <li className="text-3xl flex gap-4 pe-4">
+                <a href="https://www.instagram.com/nas4_4/" target="_blank"> <AiFillInstagram /> </a>
+                <a href="https://www.linkedin.com/in/numan-ahmed-711299282/" target="_blank"> <AiFillLinkedin /> </a>
+                <a href="https://github.com/n4hm3" target="_blank"> <AiFillGithub/> </a>
+                <a href="https://www.hackerrank.com/n4hm3" target="_blank"> <FaHackerrank/> </a>
+              </li>
+            </div>
+          */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </section>     
+      </main>
+    </div>
   )
 }
